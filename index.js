d964ab17ffe547c6d4427e6165dab345f52f240b
Domenicfc4 <125094953+Domenicfc4@users.noreply.github.com>
2026-02-11 22:50:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.post("/whatsapp", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Servidor listo en puerto ${PORT}`);
