af36f69685737a42d3f58ba788eed6a3d71cb293
Domenicfc4 <125094953+Domenicfc4@users.noreply.github.com>
2026-02-11 22:55:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -123,8 +123,6 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Servidor listo en puerto ${PORT}`);
-app.get("/", (req, res) => {
-  res.send("Bot activo ");
 });
 
 
